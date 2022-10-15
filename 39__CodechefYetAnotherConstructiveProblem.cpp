2a3d1ecd89227176698ad8bd6e6bfea7629e54f7
sanmay321 <sanmaydas2002@gmail.com>
2022-10-15 23:33:05 +0530

    39th

---
+++ 39__CodechefYetAnotherConstructiveProblem.cpp
@@ -0,0 +1,103 @@
+/*/ Author: sanmay2002(cf) sanmay320(cc) /*/
+/*/ For more details copy & check this URL-> https://www.scrolldroll.com/wp-content/uploads/2022/02/Bhai-kya-kar-raha-hai-tu-Viral-Indian-Meme-Templates-of-2022.webp           /*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                     long long int
+#define str                     string
+#define mii                     map<int,int>
+#define mll                     map<ll,ll>
+#define mci                     map<char,int>
+#define msi                     map<string,int>
+#define msl                     map<string,ll>
+#define pii                     pair<int, int>
+#define pli                     pair<ll, int>
+#define pil                     pair<int, ll>
+#define pll                     pair<ll, ll>
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vb                      vector<bool>
+#define vd                      vector<double>
+#define vs                      vector<string>
+#define vc                      vector<char>
+#define ff                      first
+#define ss                      second
+#define db                      double
+#define pb                      push_back
+#define vvi                     vector<vi>
+#define vvl                     vector<vl>
+#define vvb                     vector<vb>
+#define vvc                     vector<vc>
+#define vvs                     vector<vs>
+#define se                      set<int>
+#define re(x)                   ll x;cin>>x;
+#define ree(x,y)                ll x,y;cin>>x>>y;
+#define reee(x,y,z)             ll x,y,z;cin>>x>>y>>z;
+#define wr(x)                   cout<<x<<" ";
+#define wrr(x,y)                cout<<x<<" "<<y;
+#define wrrr(x,y,z)             cout<<x<<' '<<y<<' '<<z<<' ';
+#define yes                     cout << "YES";
+#define no                      cout << "NO";
+#define fast_io                 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define MOD                     1000000000;
+const float PI = 3.14;
+ll modd = 998244353;
+inline void inout()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ll power(int N,int R){      
+    ll temp;
+   if(R==1)return N;
+    if(R%2==0){
+        ll y=power(N,R/2);
+       return (y*y)%modd;
+    }
+    if(R%2==1){
+        temp = (N*power(N,R-1))%modd;
+    }
+            return temp;
+}
+ll bitTemp(ll n)
+{
+        int pos = 0;
+        while (n > 0) {
+            n = n >> 1;
+            pos++;
+        }
+        return pos;
+}
+//*------------------------------------------------------------------------------------*
+void solve()
+{
+    //Bhai kya kar raha hai tu
+    re(n)
+    // wr(bitTemp(n))
+    ll temp = power(2,bitTemp(n)) % MOD;        // we can use one of this of either the my created function bitTemp or log2 any one
+    // ll temp = power(2,log2(n)+1) % MOD;             
+    // wr(temp)
+    wrrr(0,n,(temp+n))
+}
+//*------------------------------------------------------------------------------------*
+inline bool isTakeTestcase()
+{
+    return true;
+    // return false;
+}
+int main()
+{
+    inout();
+    ll t=1;
+    if(isTakeTestcase ( )) cin>>t;
+    int n = t;
+    while(t--)
+    {
+        // cout<<"Case #"<<n-t<<": ";
+        solve();
+        cout<<endl;
+    }
+    return 0;
+}
